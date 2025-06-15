cb10f63bec80af194234f35b09bf3350f6597aff
devyani1512 <148792031+devyani1512@users.noreply.github.com>
2025-06-15 11:37:13 +0530

    Update socket.js

--- atlas-game-client/src/socket.js
+++ atlas-game-client/src/socket.js
@@ -1,6 +1,6 @@
 // src/socket.js
 import { io } from "socket.io-client";
 
-const socket = io("http://localhost:3000"); // Make sure this is your backend URL
+const socket = io("https://the-game-q9mr.onrender.com"); // Make sure this is your backend URL
 
 export default socket;
